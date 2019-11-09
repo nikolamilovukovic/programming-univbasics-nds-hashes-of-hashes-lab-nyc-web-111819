cc65ba406a4cd7220c8cb1e559cf48993d1b45b6
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-11-09 19:11:18 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -4,33 +4,41 @@
 #
 # The tests will guide your construction.
 #
-# PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-# 
-# PORTION_2 = {
-#   label: "Order"
-# }
-# 
-# PORTION_3 = {
-#   label: "Family",
-#   sub_category: {
-#     label: "Genus",
-#   }
-# }
-# 
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
+PORTION_1 = {
+  label: "Kingdom",
+  sub_category: {
+    label: "Phylum",
+    sub_category: {
+      label: "Class"
+    }
+  }
+}
+
+PORTION_2 = {
+  label: "Order"
+}
+
+PORTION_3 = {
+  label: "Family",
+  sub_category: {
+    label: "Genus",
+  }
+}
+
+PORTION_4 = {
+  label: "Species",
+  sub_category: nil
+}
 
 def naming_system
+  PORTION_1
+  test1 = PORTION_1
+  test1[:sub_category][:sub_category][:sub_category] = PORTION_2
+  test1[:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_3
+  test1[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category][:sub_category]= PORTION_4
+  p test1
+  
+  
   # Remember:
   #  Kingdom
   #  Phylum
