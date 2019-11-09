93645302279414374da1e49a44409b2da0bced1a
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-11-09 18:43:38 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -33,7 +33,7 @@ PORTION_4 = {
 def naming_system
   PORTION_1
   test = PORTION_1
-  test[:sub_category][:sub_category] = [:sub_category] = PORTION_2
+  test[:sub_category][:sub_category][:sub_category] = PORTION_2
   p test
   
   
