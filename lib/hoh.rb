8f97c11690fd05081a3202ce0287f194c16aa17a
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-11-09 18:57:33 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -34,7 +34,7 @@ def naming_system
   PORTION_1
   test1 = PORTION_1
   test1[:sub_category][:sub_category][:sub_category] = PORTION_2
-  test1[:sub_category][:sub_category][:sub_category] = PORTION_2
+  test1
   
   
   # Remember:
